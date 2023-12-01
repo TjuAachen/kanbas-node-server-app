566b868b062f07c2bf7e284d2083e85563fb37b7
Carl Yin <yachenyin820@gmail.com>
2023-12-01 15:48:04 -0800

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -6,18 +6,35 @@ import cors from 'cors';
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
+import UserRoutes from './users/routes.js';
+import mongoose from 'mongoose';
+import session from "express-session";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING)
 const app = express()
 app.use(express.json())
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    })
-  );
-  
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(
+  session(sessionOptions)
+);
+
+app.use(express.json());
 Lab5(app)
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Hello(app)
 app.listen(process.env.PORT || 4000);
